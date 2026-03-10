b82977e92bd0c90c20cfc8ea1d1bc98a6ce3fac7
vanleeuwenaw-dev <vanleeuwenaw@outlook.com>
2026-03-09 18:44:21 -0600

    initial commit of files

---
+++ Viewer.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace DocxViewer2
+{
+    partial class Viewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ViewerMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.FileMenuStripHeading = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenFileMenuStripItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CloseFileMenuStripItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ExitMenuStripItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ViewerStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.ViewerWebBrowser = new System.Windows.Forms.WebBrowser();
+            this.ViewerStatusStripShowFileName = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ViewerMenuStrip.SuspendLayout();
+            this.ViewerStatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ViewerMenuStrip
+            // 
+            this.ViewerMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileMenuStripHeading});
+            this.ViewerMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.ViewerMenuStrip.Name = "ViewerMenuStrip";
+            this.ViewerMenuStrip.Size = new System.Drawing.Size(800, 24);
+            this.ViewerMenuStrip.TabIndex = 0;
+            this.ViewerMenuStrip.Text = "menuStrip1";
+            // 
+            // FileMenuStripHeading
+            // 
+            this.FileMenuStripHeading.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenFileMenuStripItem,
+            this.CloseFileMenuStripItem,
+            this.toolStripSeparator1,
+            this.ExitMenuStripItem});
+            this.FileMenuStripHeading.Name = "FileMenuStripHeading";
+            this.FileMenuStripHeading.Size = new System.Drawing.Size(37, 20);
+            this.FileMenuStripHeading.Text = "File";
+            // 
+            // OpenFileMenuStripItem
+            // 
+            this.OpenFileMenuStripItem.Name = "OpenFileMenuStripItem";
+            this.OpenFileMenuStripItem.Size = new System.Drawing.Size(103, 22);
+            this.OpenFileMenuStripItem.Text = "Open";
+            this.OpenFileMenuStripItem.Click += new System.EventHandler(this.OpenFileMenuStripItem_Click);
+            // 
+            // CloseFileMenuStripItem
+            // 
+            this.CloseFileMenuStripItem.Name = "CloseFileMenuStripItem";
+            this.CloseFileMenuStripItem.Size = new System.Drawing.Size(103, 22);
+            this.CloseFileMenuStripItem.Text = "Close";
+            this.CloseFileMenuStripItem.Click += new System.EventHandler(this.CloseFileMenuStripItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(100, 6);
+            // 
+            // ExitMenuStripItem
+            // 
+            this.ExitMenuStripItem.Name = "ExitMenuStripItem";
+            this.ExitMenuStripItem.Size = new System.Drawing.Size(103, 22);
+            this.ExitMenuStripItem.Text = "Exit";
+            this.ExitMenuStripItem.Click += new System.EventHandler(this.ExitMenuStripItem_Click);
+            // 
+            // ViewerStatusStrip
+            // 
+            this.ViewerStatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ViewerStatusStripShowFileName});
+            this.ViewerStatusStrip.Location = new System.Drawing.Point(0, 428);
+            this.ViewerStatusStrip.Name = "ViewerStatusStrip";
+            this.ViewerStatusStrip.Size = new System.Drawing.Size(800, 22);
+            this.ViewerStatusStrip.TabIndex = 1;
+            this.ViewerStatusStrip.Text = "statusStrip1";
+            // 
+            // ViewerWebBrowser
+            // 
+            this.ViewerWebBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ViewerWebBrowser.Location = new System.Drawing.Point(0, 24);
+            this.ViewerWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.ViewerWebBrowser.Name = "ViewerWebBrowser";
+            this.ViewerWebBrowser.Size = new System.Drawing.Size(800, 404);
+            this.ViewerWebBrowser.TabIndex = 2;
+            // 
+            // ViewerStatusStripShowFileName
+            // 
+            this.ViewerStatusStripShowFileName.Name = "ViewerStatusStripShowFileName";
+            this.ViewerStatusStripShowFileName.Size = new System.Drawing.Size(37, 17);
+            this.ViewerStatusStripShowFileName.Text = "          ";
+            // 
+            // Viewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ViewerWebBrowser);
+            this.Controls.Add(this.ViewerStatusStrip);
+            this.Controls.Add(this.ViewerMenuStrip);
+            this.MainMenuStrip = this.ViewerMenuStrip;
+            this.Name = "Viewer";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ViewerMenuStrip.ResumeLayout(false);
+            this.ViewerMenuStrip.PerformLayout();
+            this.ViewerStatusStrip.ResumeLayout(false);
+            this.ViewerStatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip ViewerMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem FileMenuStripHeading;
+        private System.Windows.Forms.ToolStripMenuItem OpenFileMenuStripItem;
+        private System.Windows.Forms.ToolStripMenuItem CloseFileMenuStripItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem ExitMenuStripItem;
+        private System.Windows.Forms.StatusStrip ViewerStatusStrip;
+        private System.Windows.Forms.WebBrowser ViewerWebBrowser;
+        private System.Windows.Forms.ToolStripStatusLabel ViewerStatusStripShowFileName;
+    }
+}
+
---
+++ Viewer.cs
@@ -0,0 +1,181 @@
+﻿// --------------------------------
+// Copyright 2026 Anthony van Leeuwen
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+
+//       http://www.apache.org/licenses/LICENSE-2.0
+
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// ---------------------------------
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml.Packaging;
+using OpenXmlPowerTools;
+
+namespace DocxViewer2
+{
+    public partial class Viewer : Form, IViewer
+    {
+        // _filePath holds the path and name of the document to be viewed
+        private string _filePath;
+        private string _outputPath;
+
+        private string docxFilter = "Word Documents (*.docx)|*.docx|";
+        private string htmlFilter = "HTML Files (*.htm;*.html)|*.htm;*.html|";
+        private string xpsFilter = "XPS Files (*.xps)|*.xps|";
+        private string allFilesFilter = "All Files (*.*)|*.*";
+
+        private List<string> supportedExtensions = new List<string> { ".docx", ".doc", ".html", ".htm", ".xps" };
+
+        /// <summary>
+        /// Gets or sets the name of the file.
+        /// </summary>
+        /// <value>
+        /// The name of the file.
+        /// </value>
+        public string FileName
+        {
+            get { return _filePath; }
+            set { _filePath = value; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Viewer"/> class.
+        /// </summary>
+        public Viewer()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the Load event of the Form1 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(FileName))
+            {
+                string _ext = Path.GetExtension(FileName).ToLower();
+
+                if (_ext == ".docx")
+                {
+                    ConvertDocxToHtml(FileName);
+                    ViewerWebBrowser.Navigate(_outputPath);
+                }
+                else if (!supportedExtensions.Contains(_ext))
+                {
+                    MessageBox.Show("Unsupported file format.");
+                    FileName = string.Empty;
+                    ViewerStatusStripShowFileName.Text = string.Empty;
+                }
+                else
+                {
+                    ViewerWebBrowser.Url = new Uri(FileName);
+                    ViewerStatusStripShowFileName.Text = FileName.ToString();
+                }
+                ViewerStatusStripShowFileName.Text = FileName.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the OpenFileMenuStripItem control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void OpenFileMenuStripItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = docxFilter + htmlFilter + xpsFilter + allFilesFilter;
+            openFileDialog.Title = "Open Document File";
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FileName = openFileDialog.FileName;
+                string _ext = Path.GetExtension(FileName).ToLower();
+
+                if (_ext == ".docx")
+                {
+                    ConvertDocxToHtml(FileName);
+                    ViewerWebBrowser.Navigate(_outputPath);
+                }
+                else if (!supportedExtensions.Contains(_ext))
+                {
+                    MessageBox.Show("Unsupported file format.");
+                    FileName = string.Empty;
+                    ViewerStatusStripShowFileName.Text = string.Empty;
+                }
+                else
+                {
+                    ViewerWebBrowser.Url = new Uri(FileName);
+                    ViewerStatusStripShowFileName.Text = FileName.ToString();
+                }
+
+            }
+            ViewerStatusStripShowFileName.Text = FileName.ToString();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the CloseFileMenuStripItem control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void CloseFileMenuStripItem_Click(object sender, EventArgs e)
+        {
+            ViewerWebBrowser.Navigate("about:blank");
+            _outputPath = string.Empty;
+            ViewerStatusStripShowFileName.Text = string.Empty;
+        }
+
+        /// <summary>
+        /// Handles the Click event of the ExitMenuStripItem control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void ExitMenuStripItem_Click(object sender, EventArgs e)
+        {
+            CloseViewer();
+        }
+
+        /// <summary>
+        /// Closes the viewer.
+        /// </summary>
+        public void CloseViewer()
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Converts the docx to HTML.
+        /// </summary>
+        /// <param name="docxFilePath">The docx file path.</param>
+        private void ConvertDocxToHtml(string docxFilePath)
+        {
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(docxFilePath, true))
+            {
+                HtmlConverterSettings settings = new HtmlConverterSettings()
+                {
+                    PageTitle = Path.GetFileNameWithoutExtension(docxFilePath),
+                    FabricateCssClasses = true,
+                    CssClassPrefix = "docx-",
+                    RestrictToSupportedLanguages = false,
+                    RestrictToSupportedNumberingFormats = false,
+                };
+
+                var htmlElement = HtmlConverter.ConvertToHtml(wordDoc, settings);
+
+                string htmlString = htmlElement.ToString(System.Xml.Linq.SaveOptions.DisableFormatting);
+                _outputPath = Path.ChangeExtension(docxFilePath, ".html");
+                File.WriteAllText(_outputPath, htmlString, System.Text.Encoding.UTF8);
+            }
+        }
+    }
+}
